c40c1840a5f98056322cbe6671d45d33d94fd3a0
masudrana25 <mdmasudrana2512000@gmail.com>
2023-12-05 22:19:46 +0600

    VEFadgvf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules
+import React from 'react';
+import { MapContainer, TileLayer, GeoJSON, LayersControl } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
+// Example component
+const WaterResourcesMap = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <MapContainer center={[23.726215, 90.3902]}
+        zoom={10}
+        scrollWheelZoom={false}
+        style={{ width: '1000px', height: '800px' }}>
+      <LayersControl position="topright">
+        {/* Base Map Layer */}
+        <LayersControl.BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png">
+        </TileLayer>
+        </LayersControl.BaseLayer>
 
-export default App;
+        {/* GIS Data Layers */}
+        <LayersControl.Overlay name="Land Cover">
+          <TileLayer
+            attribution='Stamen Terrain Background'
+            url="https://tiles.stadiamaps.com/tiles/stamen_terrain_background/{z}/{x}/{y}{r}.png">
+        </TileLayer>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="World_Street_Map">
+         <TileLayer
+            attribution='World_Street_Map'
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}">
+        </TileLayer>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay name="landscape">
+          <TileLayer
+            attribution='landscape'
+            url="https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png">
+        </TileLayer>
+        </LayersControl.Overlay>
+      </LayersControl>
+    </MapContainer>
+  );
+};
+
+export default WaterResourcesMap;
